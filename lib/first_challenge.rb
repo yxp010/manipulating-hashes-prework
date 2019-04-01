0e88f937dec76066469989695907cb7e397c23ec
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-04-01 00:32:34 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -14,9 +14,10 @@ def first_challenge
   }
 
   #your code here
+  contacts.collect do |name, info|
+    info[favorite_icecream_flavors].delete("strawberry") if info[favorite_icecream_flavors].include?("strawberry")
+  end
 
 
   #remember to return your newly altered contacts hash!
-  contacts
 end
-
